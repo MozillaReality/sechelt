c470fbb40e30e3141d39601800ee35abf4489c95
caseyyee <caseyyee.ca@gmail.com>
2015-11-04 10:26:10 -0800

    Refactor and clean up code.

--- js/sechelt.js
+++ js/sechelt.js
@@ -1,67 +1,54 @@
-var parameters = (function() {
-	var parameters = {};
-	var parts = window.location.search.substr(1).split('&');
-	for (var i = 0; i < parts.length; i++) {
-		var parameter = parts[i].split('=');
-		parameters[parameter[0]] = parameter[1];
-	}
-	return parameters;
-})();
-
-var camera, scene, renderer;
-var controls, effect;
-var orbitControls;
-var controls2, clock = new THREE.Clock();
+var camera, scene, renderer, effect;
+var vrControls, deviceOrientation;
 var sky, water;
 var cameraPath;
 var dolly;
-var manager;
 
-function init() {
-	renderer = new THREE.WebGLRenderer({
-		antialias: true
-	});
-	renderer.autoClear = false;
-	renderer.setClearColor(0x404040);
+var currentTime = null;
+var startTime = null;
+var gotime = null;
+var speed = 20;
 
-	document.body.appendChild(renderer.domElement);
+var colorTop = new THREE.Color(0xdc72aa);
+var colorMiddle = new THREE.Color(0xfbdfd3);
+var colorBottom = new THREE.Color(0xdc72aa);
 
-	scene = new THREE.Scene();
-	scene.fog = new THREE.Fog(0xcacfde, 0, 10000);
+function setupSound() {
+	var listener = new THREE.AudioListener();
+	camera.add(listener);
 
-	dolly = new THREE.Group();
-	dolly.position.set(10000, 10000, 10000);
+	var sound = new THREE.Audio(listener);
+	sound.load('sounds/78389__inchadney__seagulls.ogg');
+	sound.position.set(475, 50, 850);
+	sound.setLoop(true);
+	sound.setRefDistance(100);
+	sound.autoplay = true;
+	scene.add(sound);
 
-	scene.add(dolly);
+	var sound = new THREE.Audio(listener);
+	sound.load('sounds/23707__hazure__seagull.ogg');
+	sound.position.set(10, 50, -200);
+	sound.setLoop(true);
+	sound.setRefDistance(100);
+	sound.autoplay = true;
+	scene.add(sound);
 
-	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 20000);
-	camera.position.z = 0.0001;
-	dolly.add(camera);
+	var sound = new THREE.Audio(listener);
+	sound.load('sounds/235428__allanz10d__calm-ocean-breeze-simulation.ogg');
+	sound.position.set(-30, 0, -750);
+	sound.setLoop(true);
+	sound.setRefDistance(100);
+	sound.autoplay = true;
+	scene.add(sound);
+}
 
-	// Effect and Controls for VR
-	effect = new THREE.VREffect(renderer);
-	controls = new THREE.VRControls(camera);
-
-	orbitControls = new THREE.OrbitControls(camera);
-
-	onWindowResize();
-
-	// Initialize the WebVR manager.
-	manager = new WebVRManager(renderer, effect);
-
-	// skybox
+function setupSkybox() {
 	var geometry = new THREE.SphereGeometry(10000, 64, 32);
-
 	var vertices = geometry.vertices;
 	var faces = geometry.faces;
 
-	var colorTop = new THREE.Color(0xdc72aa);
-	var colorMiddle = new THREE.Color(0xfbdfd3);
-	var colorBottom = new THREE.Color(0xdc72aa);
-
 	for (var i = 0, l = faces.length; i < l; i++) {
 		var face = faces[i];
-
 		var vertex1 = vertices[face.a];
 		var vertex2 = vertices[face.b];
 		var vertex3 = vertices[face.c];
@@ -76,32 +63,27 @@ function init() {
 		color3.lerp(vertex3.y > 0 ? colorTop : colorBottom, Math.abs(vertex3.y) / 6000);
 
 		face.vertexColors.push(color1, color2, color3);
-
 	}
 
 	var material = new THREE.MeshBasicMaterial({
-		//vertexColors: THREE.VertexColors,
 		side: THREE.BackSide,
 		depthWrite: false,
 		depthTest: false,
 		fog: false,
-		map: THREE.ImageUtils.loadTexture('images/bg-2.png')
+		map: new THREE.TextureLoader().load('images/bg-2.png')
 	});
 
 	sky = new THREE.Mesh(geometry, material);
 	scene.add(sky);
+}
 
-
-	// waves
+function setupWaves() {
 	var geometry = new THREE.Geometry();
-
 	var vertices = geometry.vertices;
 	var faces = geometry.faces;
-
 	var vector = new THREE.Vector3();
 
 	for (var i = 0; i < 10000; i++) {
-
 		vector.x = Math.random() * 40000 - 20000;
 		vector.z = Math.random() * 40000 - 20000;
 
@@ -129,7 +111,6 @@ function init() {
 		var c = vertices.push(vertex3) - 1;
 
 		faces.push(new THREE.Face3(a, b, c));
-
 	}
 
 	var material = new THREE.MeshBasicMaterial({
@@ -139,31 +120,11 @@ function init() {
 
 	var mesh = new THREE.Mesh(geometry, material);
 	mesh.position.y = 2.5;
-	mesh.renderDepth = 1;
+	mesh.renderOrder = 1;
 	scene.add(mesh);
+}
 
-
-	// water texture
-	// Causes massive jank and out-of-memory errors, so I abandoned this approach for time being...
-	/*
-	var texture = THREE.ImageUtils.loadTexture( 'images/waves-2.png' );
-	texture.wrapS = THREE.RepeatWrapping;
-	texture.wrapT = THREE.RepeatWrapping;
-	texture.repeat.set( 40, 40 );
-	var geometry = new THREE.PlaneGeometry( 10000, 10000, 1000, 1000 );
-	var material = new THREE.MeshBasicMaterial( {
-		//color: 0xffffff,
-		map: texture
-	} );
-	water2 = new THREE.Mesh( geometry, material );
-	water2.position.y = 3;
-	water2.rotation.x = - Math.PI / 2;
-	scene.add( water2 );
-	*/
-
-
-	// water
-
+function setupWater() {
 	var geometry = new THREE.PlaneBufferGeometry(100000, 100000);
 	var material = new THREE.MeshBasicMaterial({
 		color: colorMiddle,
@@ -173,12 +134,11 @@ function init() {
 	water = new THREE.Mesh(geometry, material);
 	water.position.y = 0;
 	water.rotation.x = -Math.PI / 2;
-	water.renderDepth = 2;
+	water.renderOrder = 2;
 	scene.add(water);
+}
 
-
-	// lights
-
+function setupLights() {
 	var directionalLight = new THREE.DirectionalLight(0xffffff, 0.15);
 	directionalLight.position.set(-1, 1, -1);
 	scene.add(directionalLight);
@@ -186,157 +146,43 @@ function init() {
 	var hemisphereLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.8);
 	hemisphereLight.position.set(-1, 2, 1.5);
 	scene.add(hemisphereLight);
+}
 
+function onModelLoaded(model) {
+	// clone and flip objects for reflection.
+	reflectObjects = ['land', 'island', 'rocks', 'trees'];
 
-	// load scene
-
-	var loader = new THREE.ObjectLoader();
-	loader.load('models/scene-nov4.json', function(object) {
-
-		var land = object.getObjectByName('land').children[0];
-		var reflection = new THREE.Mesh(land.geometry, land.material.clone());
+	reflectObjects.forEach(function(name) {
+		var object = model.getObjectByName(name).children[0];
+		var reflection = new THREE.Mesh(object.geometry, object.material.clone());
 		reflection.material.side = THREE.BackSide;
 		reflection.position.y = 0;
 		reflection.scale.y = -1;
-		land.parent.add(reflection);
-
-		var island = object.getObjectByName('island').children[0];
-		var reflection = new THREE.Mesh(island.geometry, island.material.clone());
-		reflection.material.side = THREE.BackSide;
-		reflection.position.y = 0;
-		reflection.scale.y = -1;
-		island.parent.add(reflection);
-
-		var rocks = object.getObjectByName('rocks').children[0];
-		var reflection = new THREE.Mesh(rocks.geometry, rocks.material.clone());
-		reflection.material.side = THREE.BackSide;
-		reflection.position.y = 0;
-		reflection.scale.y = -1;
-		rocks.parent.add(reflection);
-
-		var trees = object.getObjectByName('trees').children[0];
-		var reflection = new THREE.Mesh(trees.geometry, trees.material.clone());
-		reflection.material.side = THREE.BackSide;
-		reflection.position.y = 0;
-		reflection.scale.y = -1;
-		trees.parent.add(reflection);
-
-		scene.add(object);
-
-		// sounds
-
-		//var listener = new THREE.AudioListener();
-		//camera.add( listener );
-
-		/*
-		var sound = new THREE.Audio( listener );
-		sound.load( 'sounds/78389__inchadney__seagulls.ogg' );
-		sound.position.set( 475, 50, 850 );
-		sound.setLoop( true );
-		sound.setRefDistance( 100 );
-		scene.add( sound );
-
-		var sound = new THREE.Audio( listener );
-		sound.load( 'sounds/23707__hazure__seagull.ogg' );
-		sound.position.set( 10, 50, -200 );
-		sound.setLoop( true );
-		sound.setRefDistance( 100 );
-		scene.add( sound );
-
-		var sound = new THREE.Audio( listener );
-		sound.load( 'sounds/235428__allanz10d__calm-ocean-breeze-simulation.ogg' );
-		sound.position.set( -30, 0, -750 );
-		sound.setLoop( true );
-		sound.setRefDistance( 100 );
-		scene.add( sound );
-		*/
-
-		// camera path
-
-		var loader = new THREE.C4DCurveLoader();
-		loader.load('models/flightpath-nov4-bezier.txt', function(curve) {
-
-			cameraPath = curve.toLinearCurve(1); // 1 = distance between points
-
-			/*
-			// debug points
-
-			var geometry = new THREE.Geometry();
-			geometry.vertices = cameraPath.getPoints();
-			var material = new THREE.PointCloudMaterial( { size: 0.1 } );
-
-			var points = new THREE.PointCloud( geometry, material );
-			points.position.y = -1;
-			points.scale.z = -1;
-			scene.add( points );
-
-			// debug line
-
-			var geometry = new THREE.Geometry();
-			geometry.vertices = cameraPath.getPoints();
-			var material = new THREE.LineBasicMaterial();
-
-			var line = new THREE.Line( geometry, material );
-			line.position.y = -1;
-			line.scale.z = -1;
-			scene.add( line );
-			*/
-
-
-
-			requestAnimationFrame(animate);
-
-
-
-		});
-
+		object.parent.add(reflection);
 	});
 
-
-	window.addEventListener('resize', onWindowResize, false);
-	//document.body.addEventListener('click', togglePlay);
-}
-
-var playing = true;
-
-function togglePlay() {
-	playing = playing == false ? true : false;
+	scene.add(model);
 };
-
-function play() {
-	playing = true;
-}
-
-function rewind() {
-	currentTime = startTime;
-}
 
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-
-	effect.setSize(window.innerWidth, window.innerHeight);
+	renderer.setSize(window.innerWidth, window.innerHeight);
 };
 
-
-
-var currentTime = null,
-	startTime = null;
-gotime = null;
-
-var speed = 20;
-
+function startAnimation() {
+	onWindowResize();
+	requestAnimationFrame(animate);
+}
 
 function animate(time) {
-
 	requestAnimationFrame(animate);
 
 	if (cameraPath !== undefined) {
 		if (!startTime) startTime = time;
 		if (!currentTime) currentTime = time;
 
-		if (playing) currentTime += speed;
-
+		currentTime += speed;
 		gotime = TWEEN.Easing.Sinusoidal.InOut(Math.min(currentTime / 70000, 1)) * 0.9999;
 
 		var pointA = cameraPath.getPointAt(gotime);
@@ -348,26 +194,60 @@ function animate(time) {
 		dolly.position.copy(pointA);
 		dolly.lookAt(pointB);
 		dolly.rotateY(Math.PI); // look forward
-
 	}
 
-	if (controls) {
-		controls.update();
-	}
-
+	// move sky and water position with camera dolly
 	sky.position.copy(dolly.position);
 
 	water.position.x = dolly.position.x;
 	water.position.z = dolly.position.z;
 
-	if (manager.isVRMode()) {
-		effect.render(scene, camera);
-		controls.update();
-	} else {
-		renderer.render(scene, camera);
-		orbitControls.update();
-	}
+	// set rendering and controls
+	renderer.render(scene, camera);
+}
 
+function init() {
+	// setup renderer
+	renderer = new THREE.WebGLRenderer({
+		antialias: true
+	});
+	renderer.autoClear = false;
+	renderer.setClearColor(0x404040);
+	document.body.appendChild(renderer.domElement);
+
+	// setup scene
+	scene = new THREE.Scene();
+	scene.fog = new THREE.Fog(0xcacfde, 0, 10000);
+
+	// setup dolly that camera rides on.
+	dolly = new THREE.Group();
+	dolly.position.set(10000, 10000, 10000);
+	scene.add(dolly);
+
+	// setup camera
+	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 20000);
+	camera.position.z = 0.0001;
+	dolly.add(camera);
+
+	// Scene elements
+	setupSkybox();
+	setupWaves();
+	setupWater();
+	setupSound();
+	setupLights();
+
+	// load scene
+	var loader = new THREE.ObjectLoader();
+	loader.load('models/scene-nov4.json', onModelLoaded)
+
+	// load camera path
+	var loader = new THREE.C4DCurveLoader();
+	loader.load('models/flightpath-nov4-bezier.txt', function(curve) {
+		cameraPath = curve.toLinearCurve(1); // 1 = distance between points
+		startAnimation();
+	});
+
+	window.addEventListener('resize', onWindowResize, false);
 }
 
 init();
